4d989fc458f497b6fbf78a50e2e441b102e891be
wood51 <dpoulin45@gmail.com>
2025-09-09 19:31:43 +0200

    WIP: Migrations  Controle Essais ; Etats OK

---
+++ app/Models/Etat.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Etat extends Model
+{
+    protected $fillable = ['libelle','couleur','bg_color','fg_color'];
+
+    
+}
