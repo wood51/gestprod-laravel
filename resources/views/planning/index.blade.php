00935a2d42f32b8c8e52a4f10e274297da30a5b8
wood51 <dpoulin45@gmail.com>
2025-10-07 20:06:51 +0200

    WIP planning list

--- resources/views/planning/index.blade.php
+++ resources/views/planning/index.blade.php
@@ -1,5 +1,26 @@
 <div class="card w-full h-full bg-base-100 shadow-xl">
     <div class="card-body">
         <h2 class="card-title">Planning</h2>
+        <table class="table">
+            <!-- head -->
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Sous ensemble</th>
+                    <th>Job</th>
+                    <th>Favorite Color</th>
+                </tr>
+            </thead>
+            <tbody>
+                @for ($i = 0; $i < 10; $i++)
+                    <tr class="hover:bg-base-300">
+                        <th><input type="checkbox" id="row-{{ $i }}" />{{ $i }}</th>
+                        <td>Brice Swyre</td>
+                        <td>Tax Accountant</td>
+                        <td>Red</td>
+                    </tr>
+                @endfor
+            </tbody>
+        </table>
     </div>
 </div>
