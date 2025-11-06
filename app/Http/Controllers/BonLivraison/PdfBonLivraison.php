5c9f1f77124183e27ff50ea53b453365f1f60ec3
dpoulin <dpoulin45@gmail.com>
2025-11-06 16:43:55 +0100

    WIP mise en forme des BL

--- app/Http/Controllers/BonLivraison/PdfBonLivraison.php
+++ app/Http/Controllers/BonLivraison/PdfBonLivraison.php
@@ -11,11 +11,11 @@ class PdfBonLivraison
     public function makePdf(int $no_bl, BonLivraisonService $service)
     {
         $bl = BonLivraison::findOrFail($no_bl);
-        $date_bl = $bl->validated_at->format("j-m-Y");
+        $date_bl = $bl->validated_at->format("d-m-Y");
         $lignes = $service->read($no_bl);
         $html = view('bon_livraison.pdf_bl', compact('lignes', 'bl'))->render();
 
-        $pdf = new TcPdfExtend(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
         $pdf->setPrintHeader(false);
         $pdf->setPrintFooter(true);
         $pdf->SetAuthor('GESTPROD DEE');              // optionnel
@@ -26,53 +26,13 @@ class PdfBonLivraison
         $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
         $pdf->setFooterFont(['times', '', 10]);
 
-        $pdf->SetFont('times', 'B', 14);
-        $pdf->MultiCell(0, 0, "BON DE LIVRAISON N°$no_bl \ndu $date_bl", 0, 'C');
-        $pdf->loadData();
-        $pdf->Output("BL_{$no_bl}.pdf", 'I');   // 'I' pour afficher, 'D' pour télécharger
-    }
-}
-
-class TcPdfExtend extends TCPDF
-{
-    public function Footer()
-    {
-        $this->setY(-15);
-        $this->SetFont('times', '', 10);
-        $this->Cell(0, 10, 'Page ' . $this->getAliasNumPage() . '/' . $this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
-    }
-
-    public function LoadData() // FIXME Test pour debug
-    {
-        $service = new BonLivraisonService();
-        $no_bl = 1;
-        $bl = BonLivraison::findOrFail($no_bl);
-        $date_bl = $bl->validated_at->format("j-m-Y");
-        $lignes = $service->read($no_bl);
-
-        // Titre
-        echo nl2br("BON DE LIVRAISON N°$no_bl\ndu $date_bl\n\n");
-
-        // Headers
-        echo "Reference\t";
-        echo "Designation\t";
-        echo "N° Cmde\t";
-        echo "Poste\t";
-        foreach ($lignes->first()->numero_meta as $key => $value) {
-            echo $key . "\t";
-        }
+         $pdf->SetFont('times', '', 10);
+        // $pdf->MultiCell(0, 0, "BON DE LIVRAISON N°$no_bl \ndu $date_bl\n", 0, 'C');
+        $pdf->writeHTML($html,false,true,false,false,'C');
         
-        // Lignes
-        echo nl2br("\n");
-        foreach ($lignes as $ligne) {
-            echo nl2br("$ligne->article_ref\t$ligne->article_designation\t$ligne->no_commande\t$ligne->no_poste\t");
-            foreach($ligne->numero_meta as $num) {
-                echo nl2br("$num\t");
-            }
-            echo nl2br("\n");
-        }
+        $pdf->Output("BL_{$no_bl}.pdf", 'I');   // 'I' pour afficher, 'D' pour télécharger
 
 
-        die();
     }
 }
+
--- resources/views/bon_livraison/pdf_bl.blade.php
+++ resources/views/bon_livraison/pdf_bl.blade.php
@@ -1,23 +1,19 @@
-<style></style>
+<style>
 
-    <table border="1" style="width=100%">
+</style>
+
+    <table border="1"  style="width: 190mm; margin: 0 auto;">
         <thead>
             <tr>
-                <td style="height:30px;line-height: 30px;text-align:center">Reference</td>
-                <td style="height:30px;line-height: 30px;text-align:center">Désignation</td>
-                <td style="height:30px;line-height: 30px;text-align:center">N° Cmde</td>
-                <td style="height:30px;line-height: 30px;text-align:center">Poste</td>
-                <td style="height:30px;line-height: 30px;text-align:center">Qte</td>
-                <!-- <td style="height:30px;width:100px;line-height: 30px;text-align:center">Reference</td>
-                <td style="height:30px;width:160px;line-height: 30px;text-align:center">Désignation</td>
-                <td style="height:30px;width:60px;line-height: 30px;text-align:center">N° Cmde</td>
-                <td style="height:30px;width:30px;line-height: 30px;text-align:center">Poste</td>
-                <td style="height:30px;width:30px;line-height: 30px;text-align:center">Qte</td> -->
+                <!-- <td style="width:35mm;height:30px;line-height: 30px;text-align:center">Reference</td>
+                <td style="width:60mm;height:30px;line-height: 30px;text-align:center">Désignation</td> -->
+                <td style="width:15.83mm;height:30px;line-height: 30px;text-align:center">N° Cmde</td>
+                <td style="width:15.83mm;height:30px;line-height: 30px;text-align:center">Poste</td>
+                <td style="width:15.83mm;height:30px;line-height: 30px;text-align:center">Qte</td>
 
                 @if ($lignes->isNotEmpty())
                 @foreach ($lignes->first()->numero_meta as $key => $value)
-                <td style="height:30px;line-height: 30px;text-align:center">{{ $key }}</td>
-                <!-- <td style="height:30px;width:60px;line-height: 30px;text-align:center">{{ $key }}</td> -->
+                <td style="width:15.83mm;height:30px;line-height: 30px;text-align:center">{{ $key }}</td>
                 @endforeach
                 @endif
 
@@ -26,24 +22,15 @@
         <tbody>
             @foreach ($lignes as $ligne)
             <tr>
-                <td style="text-align:center">{{ $ligne->article_ref }}</td>
-                <td style="height:30px;line-height: 30px;text-align:center">{{ $ligne->article_designation }}</td>
-                <td style="text-align:center">{{ $ligne->no_commande }}</td>
-                <td style="text-align:center">{{ $ligne->no_poste }}</td>
-                <td style="text-align:center">1</td>
+                <!-- <td style="width:35mm;text-align:center">{{ $ligne->article_ref }}</td>
+                <td style="width:60mm;text-align:center">{{ $ligne->article_designation }}</td> -->
+                <td style="width:15.83mm;text-align:center">{{ $ligne->no_commande }}</td>
+                <td style="width:15.83mm;text-align:center">{{ $ligne->no_poste }}</td>
+                <td style="width:15.83mm;text-align:center">1</td>
 
                 @foreach ($ligne->numero_meta as $num)
-                <td style="text-align:center">{{ $num }}</td>
+                <td style="width:15.83mm;text-align:center">{{ $num }}</td>
                 @endforeach
-                <!-- <td style="width:100px;text-align:center">{{ $ligne->article_ref }}</td>
-                <td style="height:30px;width:160px;line-height: 30px;text-align:center">{{ $ligne->article_designation }}</td>
-                <td style="width:60px;text-align:center">{{ $ligne->no_commande }}</td>
-                <td style="width:30px;text-align:center">{{ $ligne->no_poste }}</td>
-                <td style="width:30px;text-align:center">1</td>
-
-                @foreach ($ligne->numero_meta as $num)
-                <td style="width:60px;text-align:center">{{ $num }}</td>
-                @endforeach -->
 
             </tr>
             @endforeach
