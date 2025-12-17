62293acf2a252854a029a55e967f3d4aea90f3b2
dpoulin <dpoulin45@gmail.com>
2025-12-17 11:01:50 +0100

    -

--- routes/web.php
+++ routes/web.php
@@ -21,33 +21,33 @@ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
+// Dashboard KPI
+Route::get('/kpi/dashboard', [Dashboard::class, 'showDashboard'])->name('dashboard');
+Route::post('/kpi/api/set-week', [Dashboard::class, 'setDashboardWeek']);
+
+Route::get('/kpi/api/engagement-type/{week}', [Engagement::class, 'jsonEngagementType']);
+Route::get('/kpi/api/engagement/{week}', [Engagement::class, 'jsonEngagement']);
+
+Route::get('/kpi/api/respect-engagement/{week}', [Engagement::class, 'jsonRespectEngagement']);
+Route::get('/kpi/respect-engagement/{week}', [Engagement::class, 'renderRespectEngagement']);
+
+Route::get('/kpi/api/rendement-mois/{week}/{nb_week}', [Rendement::class, 'jsonRendementMois']);
+Route::get('/kpi/rendement-mois/{week}/{nb_week}', [Rendement::class, 'renderRendementMois']);
+
+Route::get('kpi/api/qualite/{week}', [Qualite::class, 'jsonQualite']);
+Route::get('kpi/qualite/{week}', [Qualite::class, 'renderQualite']);
+
+Route::get('kpi/api/qualite-mois/{week}/{nb_week}', [Qualite::class, 'jsonQualiteMois']);
+Route::get('kpi/qualite-mois/{week}/{nb_week}', [Qualite::class, 'renderQualiteMois']);
+
 Route::middleware('auth')->group(function () {
-Route::get('/', [HomeController::class, 'showHome'])->name('home');
-
-    // Dashboard KPI
-    Route::get('/kpi/dashboard', [Dashboard::class, 'showDashboard'])->name('dashboard');
-    Route::post('/kpi/api/set-week', [Dashboard::class, 'setDashboardWeek']);
-
-    Route::get('/kpi/api/engagement-type/{week}', [Engagement::class, 'jsonEngagementType']);
-    Route::get('/kpi/api/engagement/{week}', [Engagement::class, 'jsonEngagement']);
-
-    Route::get('/kpi/api/respect-engagement/{week}', [Engagement::class, 'jsonRespectEngagement']);
-    Route::get('/kpi/respect-engagement/{week}', [Engagement::class, 'renderRespectEngagement']);
-
-    Route::get('/kpi/api/rendement-mois/{week}/{nb_week}', [Rendement::class, 'jsonRendementMois']);
-    Route::get('/kpi/rendement-mois/{week}/{nb_week}', [Rendement::class, 'renderRendementMois']);
-
-    Route::get('kpi/api/qualite/{week}', [Qualite::class, 'jsonQualite']);
-    Route::get('kpi/qualite/{week}', [Qualite::class, 'renderQualite']);
-
-    Route::get('kpi/api/qualite-mois/{week}/{nb_week}', [Qualite::class, 'jsonQualiteMois']);
-    Route::get('kpi/qualite-mois/{week}/{nb_week}', [Qualite::class, 'renderQualiteMois']);
+    Route::get('/', [HomeController::class, 'showHome'])->name('home');
 
     // Planning
     Route::get('/realisation', [RealisationController::class, 'index'])->name('realisation.index');
     Route::get('/realisation/rows', [RealisationController::class, 'rows'])->name('realisation.rows');
     Route::post('/realisations/add-to-bl', [BonLivraisons::class, 'addRealisationsToBl'])
-    ->name('realisations.addToBl');
+        ->name('realisations.addToBl');
 
     // Bon de livraisons
     route::get('bl', [BonLivraisons::class, 'index'])->name('bl.index');
@@ -63,7 +63,9 @@ Route::get('/', [HomeController::class, 'showHome'])->name('home');
     // Route::resource('commandes', CommandeController::class)
     // ->only(['index', 'show']);
 
-    route::get('/commandes',[CommandeController::class,'index'])->name('commandes.index');
-    route::get('/commandes/{commande}',[CommandeController::class,'show'])->name('commandes.show');
-    route::get('/pdp',[PdpController::class,'index'])->name('pdp.index');
+    route::get('/commandes', [CommandeController::class, 'index'])->name('commandes.index');
+    route::get('/commandes/{commande}', [CommandeController::class, 'show'])->name('commandes.show');
+    route::get('/pdp', [PdpController::class, 'index'])->name('pdp.index');
 });
+
+
